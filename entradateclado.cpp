a4a1b463b9387997646a13e45a60328bfdb8315e
zYoyi <l21212012@tectijuana.edu.mx>
2023-10-06 16:23:42 -0700

    Entrada por Teclado

---
+++ entradateclado.cpp
@@ -0,0 +1,8 @@
+int main() {
+    std::string nombre;
+    std::cout << "Por favor, ingrese su nombre: ";
+    std::cin >> nombre;
+    std::cout << "Hola, " << nombre << "! Bienvenido a este programa." << std::endl;
+
+    return 0;
+}
