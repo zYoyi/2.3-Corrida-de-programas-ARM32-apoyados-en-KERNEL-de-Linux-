c0355fd366e3448e23526f3050f71756ead23e61
Jordi Ortega <92190829+zYoyi@users.noreply.github.com>
2023-10-06 17:04:43 -0700

    Update divide.cpp

--- divide.cpp
+++ divide.cpp
@@ -1,19 +1,41 @@
+/*---------------------------------------------------------------*/
+/*			Solucion en C			   	*/
+/*--------------------------------------------------------------*/
 #include <iostream>
 
 int main() {
-    // Declarar una variable para almacenar el número ingresado por el usuario
-    double numero;
+    int numerador = 9;
+    int denominador = 3;
 
-    // Pedir al usuario que ingrese un número
-    std::cout << "Por favor, ingrese un número: ";
-    std::cin >> numero;
+    int resultado = numerador / denominador;
 
-    // Realizar la división entre 3
-    double resultado = numero / 3.0;
-
-    // Mostrar el resultado
-    std::cout << "El resultado de la división de " << numero << " entre 3 es: " << resultado << std::endl;
+    std::cout << "El resultado de la división de 9 entre 3 es: " << resultado << std::endl;
 
     return 0;
 }
 
+/*--------------------------------------------------------------*/
+/*                     Seccion de codigo			*/
+/*--------------------------------------------------------------*/
+.global main
+.func main
+.arm
+
+main:    	ADR R5, thumbcode+1     //se asigna la dirección de la etiqueta thumbcode al registro 5
+		MOV LR, PC              //se almacena la siguiente instrucción en el registro LR
+		BX R5                   //salto al registro r5
+
+exit:   	MOV R0, R3
+		MOV R7, #1
+		SWI 0
+
+.thumb
+thumbcode:      MOV R0, #9     //Se almacena el valor del numerador
+		MOV R1, #3     //Se almacena el valor del denominador
+		MOV R3, #0     //Se utiliza como contador para saber cuantas veces cabe
+		
+loop:    	ADD R3, #1    //Se aumenta el contador en 1
+		SUB R0, R1    //Se resta valor del denominador al valor del numerador
+		BGE loop      //Se preguna se el resultado es un valor mayor o igual
+		SUB R3, #1    //Resultado de la división
+		BX LR	      //Retorno a al código principal
